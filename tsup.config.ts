e289ae2a160dbabc7ddab85ee86bcd912d7627ab
smarty <mohit.patel1966@gmail.com>
2025-04-24 15:59:05 +0530

    tsup config done

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { type Options, defineConfig } from "tsup";
+const commons: Options = {
+  minify: false,
+  sourcemap: true,
+  dts: true,
+  clean: true,
+  target: "es2018",
+  external: ["react"],
+  format: ["esm", "cjs"],
+};
+export default defineConfig([
+  {
+    ...commons,
+    entryPoints: ["src/index.tsx"],
+    outDir: "dist",
+  },
+]);
